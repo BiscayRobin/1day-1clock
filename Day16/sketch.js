5b192bd54bcfad0d0e9d1c3f6f6af912dc1c6279
Robin Biscay <biscay.rob@gmail.com>
2020-06-02 01:39:48 +0200

    added color

--- Day16/sketch.js
+++ Day16/sketch.js
@@ -27,26 +27,32 @@ let strum = 1;
 
 let offsets = [0, 0, 0];
 
+let colors = ['#577399', '#577399', '#577399'];
+
 function draw() {
     let ts = new Time(new Date());
     let offsetsAdd = [ts.hour / 24, ts.minute / 60, ts.second / 60];
-    background(220);
-    stroke(4);
+    background('#F7F7FF');
+    strokeWeight(6);
     noFill();
+    push();
     for (let i = 0; i < 3; i++) {
-        strokeWeight(4);
+        stroke(colors[i]);
         beginShape();
         vertex(0, height);
         for (var x = 0; x < width / 3; x++) {
             var angle = offsets[i] + x * offsetsAdd[i] / 10;
-            var y = map(sin(angle), -strum, strum, height / 2 - 100, height / 2 + 100);
+            var y = map(sin(angle), -strum, strum, height / 2 - height / 4, height / 2 + height / 4);
             vertex(x, y);
         }
         vertex(width / 3, height);
         endShape();
         translate(width / 3, 0);
-        strokeWeight(10);
         offsets[i] += offsetsAdd[i];
-        line(0, 0, 0, height);
     }
+    pop();
+    strokeWeight(10);
+    stroke('#fe5f55');
+    line(width / 3, 0, width / 3, height);
+    line(2 * width / 3, 0, 2 * width / 3, height);
 }
